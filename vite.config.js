2c0156c05fa835df845ea1e29b4c676e7ac1ea47
Rock <rocky_oo@hotmail.com>
2025-06-18 14:38:36 +0700

    Fix Mobile Color

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   build: {
     target: 'es2019'
   },
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss(),],
 })
